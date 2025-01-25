56c3d478226c76718b0532666116626f37954009
Meri Kasprak <mkasprak@mchenry.edu>
2025-01-25 11:30:03 -0600

    knock knock joke

---
+++ print.py
@@ -0,0 +1,14 @@
+"""_summary_
+    reference: https://www.w3schools.com/python/python_strings.asp
+    used to learn to display quotes
+    knock knock joke from chatgpt:https://chatgpt.com/g/g-p-6786730b8f8c8191a6507b6104134431-add-100-update/c/67951a3e-eaac-800e-97c2-7f745134f1d3
+    Knock, knock.
+Who's there?
+Java.
+Java who?
+Java a clue how to fix this bug?
+"""
+
+print("Knock knock")
+print("Who's there?")
+print("Java", "\nJava who?", "\nJava a clue how to fix this bug?")
