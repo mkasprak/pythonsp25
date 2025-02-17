c51d04a7491f0c565db109dc9b2c6fdfd45af8f1
Meri Kasprak <mkasprak@mchenry.edu>
2025-02-17 13:31:33 -0600

    bottles_start.py

---
+++ bottles_notes.py
@@ -0,0 +1,6 @@
+# create a variable and set to 99
+# bottles = 99
+# while loop > 1
+# print the lines using the variable
+# if statement not plural (1 bottle of beer)
+# if statement 0 - (no bottles)
