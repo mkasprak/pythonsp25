d67f61965aa2f244af9765a8ac73045c540672b3
Meri Kasprak <mkasprak@mchenry.edu>
2025-02-24 12:40:06 -0600

    blood sugar

---
+++ fun.py
@@ -0,0 +1,22 @@
+"""
+Playing with Functions!
+"""
+
+
+def main():
+    start()
+    my_function()
+    start()
+
+
+def start():
+    print("Hello!")
+    print()
+    print("Hi", "Hello Again")
+
+
+def my_function():
+    print("I'm in the function!")
+
+
+main()
---
+++ user_input_demo.py
@@ -0,0 +1,15 @@
+"""
+User input demo
+
+
+"""
+
+integer_sample = int(input("Please input a whole number: "))
+
+name = input("Please enter your first name: ")
+
+float_sample = float(input("Please enter a number with a decimal: "))
+
+print(f"Integer: {integer_sample}")
+print(f"Name: {name}")
+print(f"Float: {float_sample}")
